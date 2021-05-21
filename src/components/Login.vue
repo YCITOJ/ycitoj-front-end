84d0ad443dfe09622a118808bdaeb82c6578d15b
张天伟 <3126598975@qq.com>
2021-05-21 19:59:01 +0800

    完成了登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,174 @@
 <template>
-  <div>
-    <p>登录页面</p>
+  <div class="login_container">
+    <div class="login_box">
+      <ul class="menu-tab">
+        <li
+          v-for="item in menuTab"
+          :key="item.id"
+          :class="{ current: item.current }"
+          @click="toggleMneu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+      <!-- 表单区域 -->
+      <!-- 登录表单区域 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="80px"
+        class="login_form"
+      >
+        <!-- 用户名 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <el-form-item
+          label="确认密码"
+          prop="password"
+          v-show="model === 'register'">
+          <el-input v-model="loginForm.chickpassword" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login" class="block"
+            >提交</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      menuTab: [
+        { txt: "登录", current: true, type: "login" },
+        { txt: "注册", current: false, type: "register" },
+      ],
+      model: "login",
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    toggleMneu(data) {
+      this.menuTab.forEach((elem) => {
+        elem.current = false;
+      });
+      data.current = true;
+      this.model = data.type;
+    },
+    login() {
+      if (this.model === "login") {
+        this.$refs.loginFormRef.validate(async (valid) => {
+          if (!valid) return;
+          const { data: res } = await this.$http.post("login", this.loginForm);
+          if (res.meta.status !== 200) return this.$message.error("登录失败！");
+          this.$message.success("登录成功");
+          // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+          //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+          //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+          window.sessionStorage.setItem("token", res.data.token);
+          // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+          this.$router.push("/home");
+        });
+      }
+      /* else if(this.model === "register") {
+        this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return
+        // 可以发起添加用户的网络请求
+        const { data: res } = await this.$http.post('users', this.addForm)
+
+        if (res.meta.status !== 201) {
+          this.$message.error('添加用户失败！')
+        }
+
+        this.$message.success('添加用户成功！')
+        // 隐藏添加用户的对话框
+        this.addDialogVisible = false
+        // 重新获取用户列表数据
+        this.getUserList()
+      })
+      } */
+    },
+  },
+};
+</script>
+<style scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+/* 选择登录注册样式 */
+.menu-tab {
+  text-align: center;
+}
+li {
+  display: inline-block;
+  width: 88px;
+  line-height: 36px;
+  font-size: 14px;
+  border-radius: 2px;
+  cursor: pointer;
+}
+.current {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.login_form {
+  position: absolute;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
