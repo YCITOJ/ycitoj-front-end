ad99aa4b96a236c27196ffe2e0c528ddff61d836
张天伟 <3126598975@qq.com>
2021-06-24 09:57:24 +0800

    完成了首页布局

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
     <div class="card1">
-        <div class="card1-1"></div>
-        <div class="card1-2"></div>
+      <div class="card1-1 border_style">
+        <div class="border_style_header">
+          <h4>公告</h4>
+        </div>
+        <div class="main"></div>
+      </div>
+      <div class="card1-2 border_style">
+        <div class="border_style_header">
+          <h4>排名</h4>
+        </div>
+        <div class="main"></div>
+      </div>
     </div>
     <div class="card2">
-        <div class="card2-1"></div>
-        <div class="card2-2"></div>
-        <div class="card2-3"></div>
+      <div class="card2-1 border_style">
+        <div class="border_style_header">
+          <h4>通知</h4>
+        </div>
+        <div class="main"></div>
+      </div>
+      <div class="card2-2 border_style">
+        <div class="border_style_header">
+          <h4>近期通知</h4>
+        </div>
+        <div class="main"></div>
+      </div>
+      <div class="card2-3 border_style">
+        <div class="border_style_header">
+          <h4>友情链接</h4>
+        </div>
+        <div class="main"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,38 +40,48 @@
 .card1 {
   float: left;
   margin-top: 30px;
-  margin-left: 150px;
-  width: 900px;
-  background-color: red;
+  margin-left: 17%;
+  width: 51%;
 }
 .card2 {
   float: right;
   margin-top: 30px;
-  margin-right: 100px;
-  width: 300px;
-  background-color: gold;
+  margin-right: 8%;
+  width: 23%;
 }
+/* 边框样式 */
+.border_style {
+  border: 1px solid #d4d4d5;
+  border-radius: 6px;
+}
+.border_style_header {
+  border-bottom: 1px solid #d5d4d4;
+  height: 50px;
+  background-color: #f5f3f3;
+}
+h4 {
+  margin: 0;
+  padding-left: 3%;
+  line-height: 50px;
+}
+
 .card1-1 {
-    background-color: #666;
-    height: 400px;
+  height: 400px;
 }
 .card1-2 {
-    margin-top: 50px;
-    background-color: #366;
-    height: 800px;
+  margin-top: 20px;
+  height: 800px;
 }
+
 .card2-1 {
-    background-color: #666;
-    height: 300px;
+  height: 300px;
 }
 .card2-2 {
-    margin-top: 50px;
-    background-color: #366;
-    height: 300px;
+  margin-top: 20px;
+  height: 650px;
 }
 .card2-3 {
-    margin-top: 50px;
-    background-color: #966;
-    height: 550px;
+  margin-top: 20px;
+  height: 220px;
 }
 </style>
