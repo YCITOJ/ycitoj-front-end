2422ff02e92d1ed24ac1c972d9acca3b2d39cf41
张天伟 <3126598975@qq.com>
2021-06-30 22:47:13 +0800

    主页友情链接

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -30,12 +30,29 @@
       <div class="card2-3 border_style">
         <div class="border_style_header">
           <h4>友情链接</h4>
+          <div class="box">
+            <a href="#">
+              <div class="img">
+                <img src="../../assets/img/codeforces.png" />
+              </div>
+            </a>
+            <p>codeforces</p>
+          </div>
+          <div class="box">
+            <a href="#">
+              <div class="img">
+                <img src="../../assets/img/logo-single.png" />
+              </div>
+            </a>
+            <p>落谷</p>
+          </div>
         </div>
         <div class="main"></div>
       </div>
     </div>
   </div>
 </template>
+
 <style scoped>
 .card1 {
   float: left;
@@ -78,10 +95,57 @@ h4 {
 }
 .card2-2 {
   margin-top: 20px;
-  height: 650px;
+  height: 300px;
 }
 .card2-3 {
-  margin-top: 20px;
-  height: 220px;
+  height: 400px;
+}
+
+.box {
+  float: left;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  width: 100px;
+  height: 140px;
+  margin: 35px;
+}
+
+.box .img {
+  width: 100px;
+  height: 100px;
+  box-shadow: 18px 18px 30px rgba(0, 0, 0, 0.1),
+    -18px -18px 30px rgba(255, 255, 255, 1);
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #efeeee;
+  transition: box-shadow 0.2s ease-out;
+  position: relative;
+}
+
+.box .img img {
+  width: 60px;
+  transition: width 0.2s ease-out;
+}
+
+.box p {
+  color: slategrey;
+}
+
+.box .img:hover {
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.2),
+    0px 0px 0px rgba(255, 255, 255, 0.8),
+    inset 18px 18px 30px rgba(0, 0, 0, 0.1),
+    inset -18px -18px 30px rgba(255, 255, 255, 1);
+  transition: box-shadow 0.2s ease-out;
+}
+
+.box .img:hover img {
+  width: 58px;
+  transition: width 0.2s ease-out;
 }
 </style>
