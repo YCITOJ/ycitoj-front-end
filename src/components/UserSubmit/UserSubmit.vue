c48bceac254a6909676bd4439b42a855dcf507e1
张天伟 <3126598975@qq.com>
2021-07-10 17:47:15 +0800

    完成提交页面

--- src/components/UserSubmit/UserSubmit.vue
+++ src/components/UserSubmit/UserSubmit.vue
@@ -26,87 +26,109 @@
       </el-form>
     </el-header>
     <el-main>
-        <el-card>
-         <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        label="编号"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        label="题目">
-      </el-table-column>
-      <el-table-column
-        label="状态"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        label="分数"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        label="总时间"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        label="内存"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        label="提交者"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        label="提交时间"
-        width="100">
-      </el-table-column>
-    </el-table>
-     <!-- 分页区域 -->
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.pagenum"
-        :page-size="queryInfo.pagesize"
-        layout="total, prev, pager, next, jumper"
-        :total="total">
-      </el-pagination>
-    </el-card>
+      <el-card>
+        <el-table :data="resultslist" style="width: 100%">
+          <el-table-column label="编号" width="100" prop="id"> </el-table-column>
+          <el-table-column label="题目"> </el-table-column>
+          <el-table-column label="状态" width="100" prop="verdict"> </el-table-column>
+          <el-table-column label="分数" width="100"> </el-table-column>
+          <el-table-column label="总时间" width="100" prop="cpu_time"> </el-table-column>
+          <el-table-column label="内存" width="100" prop="memory"> </el-table-column>
+          <el-table-column label="提交者" width="100"> </el-table-column>
+          <el-table-column label="提交时间" width="100" prop="create_time"> </el-table-column>
+        </el-table>
+        <!-- 分页区域 -->
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-size="queryInfo.pagesize"
+          layout="total, prev, pager, next, jumper"
+          :total="total">
+        </el-pagination>
+      </el-card>
     </el-main>
   </el-container>
 </template>
 <script>
 export default {
-    data() {
-      return {
-        formInline: {
-          problem: '',
-          user: '',
-          region: '',
-          state: ''
-        },
-    // 获取用户列表的参数对象
+  data() {
+    return {
+      formInline: {
+        problem: "",
+        user: "",
+        region: "",
+        state: "",
+      },
+      // 获取用户列表的参数对象
       queryInfo: {
-        query: "",
+        query: "10",
         // 当前的页数
         pagenum: 1,
         // 当前每页显示多少条数据
-        pagesize: 2,
+        pagesize: 5,
       },
+      resultslist:[],
+      total: 1,
+      // 获取提交页面的请求数据
+      condition_group1:{
+        condition: 'prob_id=1 and who=3'
+      },
+      condition_group2:{
+        page_no: '1',
+        condition: 'who=1'
       }
+    };
+  },
+  created(){
+    this.getPageinfo(),
+    this.getshow_per_page()
+    this.getresultslist()
+  },
+  methods: { 
+    // 获取提交列表
+    async getresultslist() {
+      const { data: res } = await this.$http.get("submit/get_submissions?page_no="+this.condition_group2.page_no+"&condition="+this.condition_group2.condition);
+      /* if (res.meta.status !== 200) {
+        return this.$message.error("获取题目列表表失败！");
+      } */
+      console.log(res)
+      this.resultslist= res.data;
     },
-    methods: {
-      onSubmit() {
-        console.log(this.region)
-        console.log('submit!');
+    // 提交页数以及提交数量
+    async getPageinfo() {
+      const { data: res } = await this.$http.get("submit/submission_info?condition="+this.condition_group1.condition);
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取提交表数量失败！");
       }
-    }
+      console.log(res)
+      this.total = res.sub_cnt;
+    },
+    // 提交每一页的题目数量
+    async getshow_per_page() {
+      const { data: res } = await this.$http.get("submit/show_per_page/");
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取提交表题目失败！");
+      }
+      console.log(res);
+      this.queryInfo.pagesize = res.data;
+    },
+    // 监听 页码值 改变的事件
+    handleCurrentChange(newPage) {
+      // console.log(newPage);
+      this.queryInfo.pagenum = newPage;
+      this.getUserList();
+    },
+    onSubmit() {
+      console.log(this.region);
+      console.log("submit!");
+    },
+  },
 };
 </script>
 <style scoped>
 .page {
-    padding-top: 20px;
-    padding-left: 10%;
-    padding-right: 10%;
+  padding-top: 20px;
+  padding-left: 5%;
+  padding-right: 5%;
 }
 </style>
