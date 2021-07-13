e085800bdc7ffc7d639c5e8369a5c6e54f28f3d0
张天伟 <3126598975@qq.com>
2021-07-13 12:28:16 +0800

    优化

--- src/components/UserSubmit/UserSubmit.vue
+++ src/components/UserSubmit/UserSubmit.vue
@@ -2,20 +2,20 @@
   <el-container class="page">
     <el-header>
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
-        <el-form-item label="题目:">
-          <el-input v-model="formInline.problem"></el-input>
+        <el-form-item label="题目编号:">
+          <el-input size="small" style="width: 100px" v-model="formInline.problem" ></el-input>
         </el-form-item>
         <el-form-item label="提交者:">
-          <el-input v-model="formInline.user"></el-input>
+          <el-input size="small" style="width: 140px" v-model="formInline.user" ></el-input>
         </el-form-item>
         <el-form-item label="语言:">
-          <el-select v-model="formInline.region">
+          <el-select size="small" style="width: 80px" v-model="formInline.region" >
             <el-option label="CPP" value="shanghai"></el-option>
             <el-option label="JAVA" value="beijing"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="状态:">
-          <el-select v-model="formInline.state">
+          <el-select  size="small" style="width: 80px" v-model="formInline.state">
             <el-option label="wo" value="shanghai"></el-option>
             <el-option label="ac" value="beijing"></el-option>
           </el-select>
@@ -27,28 +27,49 @@
     </el-header>
     <el-main>
       <el-card>
-        <el-table :data="resultslist" style="width: 100%">
-          <el-table-column label="编号" width="100" prop="id"> </el-table-column>
+        <el-table
+          :data="resultslist"
+          style="width: 100%"
+          @row-click="dialogcode"
+        >
+          <el-table-column label="编号" width="100" prop="id">
+          </el-table-column>
           <el-table-column label="题目"> </el-table-column>
-          <el-table-column label="状态" width="100" prop="verdict"> </el-table-column>
+          <el-table-column label="状态" width="100" prop="verdict">
+          </el-table-column>
           <el-table-column label="分数" width="100"> </el-table-column>
-          <el-table-column label="总时间" width="100" prop="cpu_time"> </el-table-column>
-          <el-table-column label="内存" width="100" prop="memory"> </el-table-column>
+          <el-table-column label="总时间" width="100" prop="cpu_time">
+          </el-table-column>
+          <el-table-column label="内存" width="100" prop="memory">
+          </el-table-column>
           <el-table-column label="提交者" width="100"> </el-table-column>
-          <el-table-column label="提交时间" width="100" prop="create_time"> </el-table-column>
+          <el-table-column label="提交时间" width="100" prop="create_time">
+          </el-table-column>
         </el-table>
         <!-- 分页区域 -->
         <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.pagenum"
-        :page-size="queryInfo.pagesize"
-        layout="total, prev, pager, next, jumper"
-        :total="total"
-      >
-      </el-pagination>
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-size="queryInfo.pagesize"
+          layout="total, prev, pager, next, jumper"
+          :total="total"
+        >
+        </el-pagination>
       </el-card>
     </el-main>
+    <el-dialog title="代码" :visible.sync="displayedcode">
+        <mavon-editor
+          class="md"
+          v-model="value"
+          :subfield="false"
+          :defaultOpen="'preview'"
+          :toolbarsFlag="false"
+          :editable="false"
+          :scrollStyle="true"
+          :ishljs="true"
+        />
+    </el-dialog>
   </el-container>
 </template>
 <script>
@@ -69,48 +90,60 @@ export default {
         // 当前每页显示多少条数据
         pagesize: 5,
       },
-      resultslist:[],
+      resultslist: [],
       total: 1,
       // 获取提交页面的请求数据
-      condition_group1:{
-        condition: 'prob_id=1 and who=1'
+      condition_group1: {
+        condition: "prob_id=1 and who=1",
       },
-      condition_group2:{
+      condition_group2: {
         page_no: 1,
-        condition: 'who=1'
-      }
+        condition: "who=1",
+      },
+      //代码的显示
+      displayedcode: false,
+      //代码内容
+      value:"",
     };
   },
-  created(){
-    this.getPageinfo(),
-    this.getshow_per_page(),
-    this.getresultslist()
+  created() {
+    this.getPageinfo(), this.getshow_per_page(), this.getresultslist();
   },
-  methods: { 
+  methods: {
     // 获取提交列表
     async getresultslist() {
-      this.condition_group2.page_no = this.queryInfo.pagenum 
-      const { data: res } = await this.$http.get("submit/get_submissions?page_no="+this.condition_group2.page_no+"&condition="+this.condition_group2.condition);
-      /* if (res.meta.status !== 200) {
-        return this.$message.error("获取题目列表表失败！");
-      } */
+      this.condition_group2.page_no = this.queryInfo.pagenum;
+      const { data: res } = await this.$http.get(
+        "submit/get_submissions?page_no=" +
+          this.condition_group2.page_no +
+          "&condition=" +
+          this.condition_group2.condition
+      );
+      if (res.meta.status !== 200) {
+        return console.log("获取题目列表表失败！");
+      }
       //console.log(res)
-      this.resultslist= res.data;
+      this.resultslist = res.data;
     },
     // 提交页数以及提交数量
     async getPageinfo() {
-      const { data: res } = await this.$http.get("submit/submission_info?condition="+this.condition_group1.condition);
-      if (res.meta.status !== 200) {
-        return this.$message.error("获取提交表数量失败！");
+      const { data: res } = await this.$http.get(
+        "submit/submission_info?condition=" + this.condition_group1.condition
+      );
+      if (res.meta.status === 403) {
+        return this.$message.error("请先登录！");
       }
-      console.log(res)
+      if (res.meta.status !== 200) {
+        return console.log("获取提交表数量失败！");
+      }
+      //console.log(res);
       this.total = res.data.sub_cnt;
     },
     // 提交每一页的题目数量
     async getshow_per_page() {
-      const { data: res } = await this.$http.get("submit/show_per_page/");
+      const { data: res } = await this.$http.get("submit/show_per_page");
       if (res.meta.status !== 200) {
-        return this.$message.error("获取提交表题目失败！");
+        return console.log("获取提交表题目失败！");
       }
       //console.log(res);
       this.queryInfo.pagesize = res.data;
@@ -127,6 +160,11 @@ export default {
       this.queryInfo.pagenum = newPage;
       this.getresultslist();
     },
+    // 显示提交的代码
+    dialogcode(row) {
+      this.displayedcode = true;
+      this.value=row.code;
+    },
     onSubmit() {
       console.log(this.region);
       console.log("submit!");
@@ -137,7 +175,8 @@ export default {
 <style scoped>
 .page {
   padding-top: 20px;
-  padding-left: 5%;
-  padding-right: 5%;
+  padding-left: 15%;
+  padding-right: 15%;
 }
+
 </style>
