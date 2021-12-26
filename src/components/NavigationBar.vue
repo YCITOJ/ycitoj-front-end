426d136d8fcb8204126f272daf20e79887fad598
ubuntu <ubuntu@localhost.localdomain>
2021-12-26 21:16:15 +0800

    update

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -20,7 +20,7 @@
           <el-menu-item index="contest">比赛</el-menu-item>
           <el-menu-item index="usersubmit">提交记录</el-menu-item>
           <el-menu-item index="class">班级</el-menu-item>
-          <el-menu-item index="admin" v-show="userlevel==0">后台</el-menu-item>
+          <el-menu-item index="admin" v-show="userlevel==1">后台</el-menu-item>
           <el-menu-item class="signin" index="login" v-show="loginname === 'in'">登录|注册</el-menu-item>
           <el-menu-item class="signin" index="user" v-show="loginname === 'out'">
             <i class="el-icon-user-solid"></i>
