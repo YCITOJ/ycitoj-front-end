b8855b2541bd669aa34cdc88165a21d140523b47
张天伟 <3126598975@qq.com>
2022-01-04 20:34:57 +0800

    优化

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //baseUrl : 'https://oj.merdog.cn:4001/'
-    baseUrl : 'https://mini-code.39nat.com/'
+  baseUrl : 'https://oj.merdog.cn:4001/'
+    //baseUrl : 'https://mini-code.39nat.com/'
     //  baseUrl : 'https://oj.merdog.cn:4001'
     //  baseUrl : 'https://101.34.240.163:443'
 }
